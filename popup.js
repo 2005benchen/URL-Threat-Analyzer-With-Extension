90a5a678854eb9276479be9eff4de9d8910aea7f
Ben Chen <111568593+2005benchen@users.noreply.github.com>
2023-08-30 15:31:20 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -5,20 +5,28 @@ chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
   const url = activeTab.url;
   const resultElement = document.getElementById('resultText');
 
-     // Simulating an API response for demonstration purposes
-     fetch('https://super02.me/predict?url=' + url, {method: 'POST'}).then(function(response) {
-      if(response.ok) {
-        response.json().then(function(data) {
-          const isMalicious = data;
-          console.log(isMalicious)
+  // Simulating an API response for demonstration purposes
+  fetch('https://super02.me/predict?url=' + url, { method: 'POST' }).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        const isMalicious = data;
+        console.log(isMalicious);
 
-          const result = isMalicious ? 'Benign' : 'Malicious';
-          console.log(result)
+        const result = isMalicious ? 'Benign' : 'Malicious';
+        console.log(result);
 
-          resultElement.textContent = result;
-        });
+        resultElement.textContent = result;
+
+        // Set styles based on the result
+        if (isMalicious) {
+          resultElement.style.color = 'green';    // Set text color to green for benign
         } else {
-          resultElement.textContent = "Error";
+          resultElement.style.color = 'red';  // Set text color to red for malicious
         }
-    });
+      });
+    } else {
+      resultElement.textContent = 'Error';
+    }
+  });
 });
+
